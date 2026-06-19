886e1e9b4ec6874d83fd6e0011129828a2657fd5
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-19 19:06:53 +0300

    save 19.06

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface Todo {
   id: string;
@@ -8,17 +8,20 @@ interface Todo {
 }
 
 function App() {
-  const [text, setText] = useState<string>("");
+  console.log("App компонент відрендерився");
+
   const [todos, setTodos] = useState<Todo[]>(() => {
     const storeTodos = localStorage.getItem("todos");
 
     return storeTodos ? JSON.parse(storeTodos) : [];
   });
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const [isSorted, setIsSorted] = useState<boolean>(false);
 
   const handleAddTodo = () => {
-    const newText = text.trim();
+    const newText = inputRef.current.value;
     if (!newText) return;
 
     const newTodo: Todo = {
@@ -29,16 +32,19 @@ function App() {
     };
 
     setTodos([...todos, newTodo]);
+    inputRef.current.value = "";
+    inputRef.current.focus();
   };
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
+    inputRef.current.focus();
   }, [todos]);
 
   return (
     <>
       <h1 className="text-center text-[32px] text-red-300 font-bold">
-        Todo List - {text}
+        Todo List
       </h1>
 
       <div className="border-2 border-yellow-400 p-4">
@@ -46,8 +52,7 @@ function App() {
           className="p-2 grow w-100 mb-3"
           type="text"
           placeholder="Что нужно сделать?"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
+          ref={inputRef}
         />
         <button
           className="bg-blue-400 px-5 py-2 rounded-3xl me-3"
